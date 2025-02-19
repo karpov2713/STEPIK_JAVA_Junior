4b8bd944c4ce532eae343f57c0d06955b26661f4
karpov2713 <karpov2713@gmail.com>
2025-02-20 01:32:07 +0300

    3.3 Параметризированные методы

--- STEPIK_JJun_OOP/src/Main.java
+++ STEPIK_JJun_OOP/src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 //        Box box1 = new Box();
-//        box1.height = 10;
-//        box1.length = 10;
-//        box1.width = 10;
-//
+//        box1.setDimens(10, 10, 10);
 //        Box box2 = new Box();
-//        box2.height = 20;
-//        box2.length = 20;
-//        box2.width = 20;
-//
+//        box2.setDimens(20, 20, 20);
 //        box1.showVolume();
 //        box2.showVolume();
+
+        Rectangle rect = new Rectangle();
+        rect.setDimens(10, 5);
+        System.out.println(rect.rectArea());
 
 //        Person john = new Person();
 //        john.name = "John";
@@ -31,12 +29,15 @@ public class Main {
 //        double summAge = john.age + ann.age + dima.age;
 //        System.out.println(summAge / 3);
 
-        Dog dog = new Dog();
-        dog.name = "Tuzik";
-        dog.breed = "Taxa";
-        dog.weight = 5;
-        dog.speed = 10;
-        //System.out.println(dog.getInfo());
-        dog.run();
+//        Dog dog = new Dog();
+//        dog.name = "Tuzik";
+//        dog.breed = "Taxa";
+//        dog.weight = 5;
+//        dog.speed = 10;
+//        //System.out.println(dog.getInfo());
+//        dog.run();
+
+//        Test test = new Test();
+//        System.out.println(test.square(20));
     }
 }
---
+++ STEPIK_JJun_OOP/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    double length;
+    double width;
+
+    void setDimens(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    double rectArea() {
+        return this.length * this.width;
+    }
+}
